8668e0cc92ad01f9ec32ec7ee441af8c72f79588
NomadMIke <nomadmike@yahoo.com>
2019-01-13 14:28:49 -0500

    Added a controller.

---
+++ src/main/java/com/mjk/HolaMundo/controller/HolaMundoController.java
@@ -0,0 +1,34 @@
+package com.mjk.HolaMundo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HolaMundoController {
+	
+	@RequestMapping("/")
+	public ModelAndView index() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
+	
+	@RequestMapping("/welcome")
+	public ModelAndView holaMundo() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
+	
+	 @GetMapping("/greeting")
+	    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+	        model.addAttribute("name", name);
+	        return "greeting";
+	    }
+}
